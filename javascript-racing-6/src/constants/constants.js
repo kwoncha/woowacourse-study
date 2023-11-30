98e7cafcbb7d14fd94864a9d91f06597c2f4601c
kwoncha <rnjsckgus@gmail.com>
2023-11-30 15:08:55 +0900

    refactor: 유저이름 받는 정규식 수정

--- javascript-racing-6/src/constants/constants.js
+++ javascript-racing-6/src/constants/constants.js
@@ -1,7 +1,7 @@
 import deepFreeze from '../utils/deepFreeze/deepFreeze.js';
 
 const REGEXS = deepFreeze({
-  userName: /^ ([a - zA - Z]{ 1, 5})(, [a - zA - Z]{ 1, 5}) * $ /,
+  userName: /^([a-zA-Z]{1,5})(,[a-zA-Z]{1,5})*$/,
   playTime: /^\d+$/,
 });
 
