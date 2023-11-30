c0ef2c791970856bb01e315ec73260bbb5234558
kwoncha <rnjsckgus@gmail.com>
2023-11-30 15:43:14 +0900

    refactor: 오타 수정

--- javascript-racing-6/src/domain/randomNumber.js
+++ javascript-racing-6/src/domain/randomNumber.js
@@ -2,7 +2,7 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 import { NUMBERS } from '../constants/constants.js';
 
 const randomNumber = () => {
-  return MissionUtils.Random.pickNumberInRange(NUMBERS.minimun, NUMBERS.maximun);
+  return MissionUtils.Random.pickNumberInRange(NUMBERS.minimum, NUMBERS.maximum);
 };
 
 export default randomNumber;
